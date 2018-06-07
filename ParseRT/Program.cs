6c49d6eb15e99b0efef56392655ff05567f763a5
Adrian Sims <adriansims62781@gmail.com>
2018-06-07 22:11:36 +0100

    Changes

--- ParseRT/Program.cs
+++ ParseRT/Program.cs
@@ -14,15 +14,30 @@ namespace ParseRT
         string ccstFormat;
         string ticketType;
         bool valid = false;
-        public RcsFtot(string ticketType, IEnumerable<string> channelList, IEnumerable<string> ccstFormat)
+        public RcsFtot(string ticketType, IEnumerable<string> channelList, IEnumerable<(string ccstFormat, string endDate, string startDate)> ccstFormats)
         {
-            if (!Regex.Match(ticketType, "[A-Z][0-9]{3}").Success)
+            if (!Regex.Match(ticketType, "[A-Z0-9]{3}").Success)
             {
                 throw new ElementException("Bad ticket type");
             }
+            if (ccstFormat.Count() > 1)
+            {
+                throw new ElementException($"More than one CCST-x format for ticket type {ticketType}");
+            }
+            foreach (var channel in channelList)
+            {
+                if (channel == "00004")
+                {
+                    valid = true;
+                }
+            }
+            if (valid)
+            {
+                this.ticketType = ticketType;
+                this.ccstFormat = ccstFormat.First();
+            }
         }
         public (string ftot, string ccstFormat, bool valid) CcstMapping => (ticketType, ccstFormat, valid);
-        public bool Valid => true;
     }
 
     internal class Program
@@ -32,10 +47,28 @@ namespace ParseRT
             try
             {
                 var files = Directory.GetFiles(@"s:\", "RCS_R_T*.xml").OrderBy(x => x.Substring(8, 6).Reverse().ToString()).ToList();
-                var filename = files.First();
+                var filename = files.Any() ?  files.First() : throw new Exception("No RCS_R_T* files found");
                 var doc = XDocument.Load(filename);
+                var rootNamespace = doc.Root.GetDefaultNamespace();
 
-                var result = doc.Descendants("FTOT").Select(x =>
+
+                var res = from ftotElement in doc.Descendants(rootNamespace + "FTOT")
+                          let ftot = ftotElement?.Attribute("t")?.Value
+                          let ccst = (format: ftotElement.Elements
+                          let filterLtypes = from licenseeTypeElement in ftotElement.Elements("LicenseeType")
+                                             let lt = licenseeTypeElement.Attribute("lt")?.Value
+                                             where lt == "00001"
+                                                 from channel in licenseeTypeElement.Descendants("Channel")
+                                                  let ch = channel.Attribute("ch")?.Value
+                                                  where ch == "00004"
+                                             select new { ftot,  };
+
+
+
+
+
+
+var result = doc.Descendants(rootNamespace + "FTOT").Select(x =>
                 {
                     RcsFtot mapping = null;
                     try
@@ -43,20 +76,29 @@ namespace ParseRT
                         mapping = new RcsFtot
                         (
                             ticketType: x.Attribute("t")?.Value,
-                            channelList: x.Descendants("Channel")?.Select(y => y.Attribute("ch")?.Value),
-                            ccstFormat: x.Descendants("CCSTFormat")?.Select(y => y.Attribute("f")?.Value)
+                            channelList: x.Descendants(rootNamespace + "Channel")?.Select(y => y.Attribute("ch")?.Value),
+                            ccstFormats: x.Descendants(rootNamespace + "Traditional")?.Select(y =>
+                            (
+                                ccstFormat: y.Elements("CCSTFormat").Select(z=>z.Attribute("f").Value).SingleOrDefault() ?? throw new ElementException("More than one CCST element defined"),
+                                startDate: y.Attribute("f")?.Value,
+                                endDate: y.Attribute("u")?.Value
+                            ))
                         );
                     }
+            
                     catch (ElementException ex)
                     {
                         Console.WriteLine($"Error in element: {ex.Message}");
                     }
                     return mapping;
-                }).Where(y => y.Valid).ToLookup(z => z.CcstMapping.ftot);
-                var dups = result.Where(x => x.Count() > 1);
-                if (dups.Any())
+                }).Where(y => y.CcstMapping.valid).ToLookup(z => z.CcstMapping.ftot);
+                var dups = result.Where(x => x.Count() > 1).ToList();
+                dups.ForEach(x => Console.WriteLine($"Duplicate ticket type found: {x.Key}"));
+
+                foreach (var mapping in result)
                 {
-                    Console.WriteLine($"Duplicate ticket type found: {}");
+                    var ccstMapping = mapping.First().CcstMapping;
+                    Console.WriteLine($"{ccstMapping.ftot} is {ccstMapping.ccstFormat}");
                 }
             }
             catch (Exception ex)
